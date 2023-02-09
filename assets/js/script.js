08e4cd2bac0492c4a6c627e29b21363a7e65cc4b
Allyson Gonzales <Allysonmg21@gmail.com>
2023-02-08 19:28:12 -0800

    Pushing for assistance

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,32 +2,44 @@ var searchCityBtn = document.getElementById("searchCityBtn");
 var userCity = document.getElementById("inputSearch");
 var currentTimeEl = document.querySelector(".currentTime");
 var temperatureEl = document.querySelector(".temperature");
+var cardGroupEl = document.querySelector(".card-header");
 
+var data = localStorage.getItem("inputSearch");
+console.log(data);
+//current time
 var time = dayjs().format("hh:mm A");
 $(".currentTime").text(time);
 
-function displayWeather(event) {
-  event.preventDefault();
-  // temperatureEl =
-}
+//5 day
+$(function () {
+  var fiveDay = dayjs().format("MMM D YYYY");
+  $(".card-header").html(fiveDay);
+});
 
 function getLonLatApi(event) {
   event.preventDefault();
   var city = userCity.value.trim();
   var apiKey = "b669ae4ac48bc41896b2dabb9c94a7ff";
-  var requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&limit=5&appid=${apiKey}`;
+  var requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&limit=5&appid=${apiKey}`;
   console.log("city", city);
 
   fetch(requestUrl).then(function (response) {
     if (response.ok)
-      return response.json().then(function (response) {
-        console.log("response", response);
-      });
+      return response
+        .json()
+        .then(function (response) {
+          console.log("response", response);
+        })
+        .then((data) => {
+          var temp = (data) => {
+            temperatureEl.textContent = ".temperature";
+          };
+        });
   });
 }
-function weatherTemp(data) {}
 
 searchCityBtn.addEventListener("submit", getLonLatApi);
+
 // console.log("this", getLonLatApi );
 // $(function () {
 //   var todaysDate = dayjs().format("hh:mm a");
