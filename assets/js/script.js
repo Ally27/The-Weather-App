953e407c79a95423423ae265fef561c1801d1d4f
Allyson Gonzales <Allysonmg21@gmail.com>
2023-02-25 11:00:37 -0800

    Pushing for assistance

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,14 +6,13 @@ var currentTimeEl = document.querySelector(".currentTime");
 var temperatureEl = document.querySelector(".temperature");
 var cardGroupEl = document.querySelector(".card-header");
 var cheaderEl = document.getElementById("cheader");
-var previousSearchContainerEl = document.getElementById(
-  "previousSearchContainer"
-);
-var previousSearches = JSON.parse(localStorage.getItem("previousSearches"));
+var pastSearchesEl = document.getElementById("pastSearches");
 var fiveDay = 5;
+var previousSearches = JSON.parse( localStorage.getItem( 'previousSearches' ) ) || [];
+
 
 //current time
-var time = dayjs().format("MMM d, hh:mm A");
+var time = dayjs().format("MMM D, hh:mm:ss A");
 $(".currentTime").text(time);
 
 //5 day
@@ -21,34 +20,28 @@ $(function () {
   var fiveDay = dayjs().format("MMM D YYYY");
   $(".card-header").html(fiveDay);
   for (var i = 0; i < fiveDay.length; i++) return i;
-  console.log(i);
 });
 //calling api with local storage on temp
 function getLocationApi(event) {
+  // console.log(getLocationApi);
   event.preventDefault();
   var city = userCity.value.trim();
   var apiKey = "b669ae4ac48bc41896b2dabb9c94a7ff";
-  var requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&count=5&appid=${apiKey}`;
+  var requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&limit=5&appid=${apiKey}`;
   //fetches the api
   fetch(requestUrl).then(function (response) {
     if (response.ok)
       return response.json().then(function (response) {
-        console.log("response", response);
-        console.log("response", response.list[0].main.temp);
+        // console.log("response", response);
+        // console.log("response", response.list[0].main.temp);
         temperatureEl.textContent = response.list[0].main.temp;
         localStorage.setItem("temp", response.list[0].main.temp);
       });
   });
 }
+
 // attempting to create location history
 function saveSearches() {
-  localStorage.setItem("previousSearches", JSON.stringify(previousSearches));
+  localStorage.setItem("pastSearches", JSON.stringify(pastSearchesEl));
 }
 searchCityBtn.addEventListener("submit", getLocationApi);
-
-// console.log("this", getLonLatApi );
-// $(function () {
-//   var todaysDate = dayjs().format("hh:mm a");
-//   $(".currentTime").html(todaysDate);
-//   setInterval(currentTimeEl);
-// });
