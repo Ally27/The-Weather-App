10b3d96d62850c01315e0e140c3fae0b7b3f238c
Allyson Gonzales <Allysonmg21@gmail.com>
2023-02-25 11:49:01 -0800

    Fixed LocalStorage for past city search

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,10 +6,9 @@ var currentTimeEl = document.querySelector(".currentTime");
 var temperatureEl = document.querySelector(".temperature");
 var cardGroupEl = document.querySelector(".card-header");
 var cheaderEl = document.getElementById("cheader");
-var pastSearchesEl = document.getElementById("pastSearches");
 var fiveDay = 5;
-var previousSearches = JSON.parse( localStorage.getItem( 'previousSearches' ) ) || [];
-
+var previousSearches =
+  JSON.parse(localStorage.getItem("previousSearches")) || [];
 
 //current time
 var time = dayjs().format("MMM D, hh:mm:ss A");
@@ -28,15 +27,17 @@ function getLocationApi(event) {
   var city = userCity.value.trim();
   var apiKey = "b669ae4ac48bc41896b2dabb9c94a7ff";
   var requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&limit=5&appid=${apiKey}`;
+
   //fetches the api
   fetch(requestUrl).then(function (response) {
-    if (response.ok)
-      return response.json().then(function (response) {
-        // console.log("response", response);
-        // console.log("response", response.list[0].main.temp);
-        temperatureEl.textContent = response.list[0].main.temp;
-        localStorage.setItem("temp", response.list[0].main.temp);
-      });
+    if (response.ok) previousSearches.push(city);
+    localStorage.setItem("previousSearches", JSON.stringify(previousSearches));
+    return response.json().then(function (response) {
+      // console.log("response", response);
+      // console.log("response", response.list[0].main.temp);
+      temperatureEl.textContent = response.list[0].main.temp;
+      localStorage.setItem("temp", response.list[0].main.temp);
+    });
   });
 }
 
